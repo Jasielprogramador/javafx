616149c27a67deda2c8ec75bcd0bb32aa69e6b6b
Asi <asierrosa@gmail.com>
2020-10-21 11:49:35 +0200

    xd

---
+++ src/main/java/ehu/isad/controllers/db/ZerbitzuKud.java
@@ -0,0 +1,57 @@
+package ehu.isad.controllers.db;
+
+import ehu.isad.controller.ui.KautotuKud;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZerbitzuKud {
+
+    private static final ZerbitzuKud instance = new ZerbitzuKud();
+
+    public static ZerbitzuKud getInstance() {
+        return instance;
+    }
+
+    private ZerbitzuKud() {
+    }
+
+    public List<String> lortuZerbitzuak() {
+
+        String query = "select id, izena from services";
+        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
+        ResultSet rs = dbKudeatzaile.execSQL(query);
+
+        List<String> emaitza = new ArrayList<>();
+        try {
+            while (rs.next()) {
+
+                int kodea = rs.getInt("id");
+                String izena = rs.getString("izena");
+
+                System.out.println(kodea + ":" + izena);
+                emaitza.add(izena);
+
+            }
+        } catch(SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        return emaitza;
+    }
+
+    public void kenduZerbitzua(String s){
+        String query = "delete from zerbitzuak.services where izena='"+s+"'";
+        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
+        dbKudeatzaile.execSQL(query);
+    }
+
+    public void gehitu(String s){
+        String query = "insert into zerbitzuak.services (izena) values ('"+s+"');";
+        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
+
+        dbKudeatzaile.execSQL(query);
+    }
+}
---
+++ src/main/java/ehu/isad/controllers/ui/KautotuKud.java
@@ -0,0 +1,70 @@
+package ehu.isad.controllers.ui;
+
+import ehu.isad.Main;
+import ehu.isad.controller.db.ZerbitzuKud;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class KautotuKud implements Initializable {
+
+    // Reference to the main application.
+    private Main mainApp;
+
+    @FXML
+    private ComboBox comboZerbitzua;
+
+    @FXML
+    private TextField txtErabiltzaile;
+
+    @FXML
+    private PasswordField txtPasahitza;
+
+
+    @FXML
+    private Button kautotuBotoia;
+
+    @FXML
+    private Button kautotuBotoia2;
+
+
+    @FXML
+    void onClick2(ActionEvent event) throws SQLException {
+        comboZerbitzua.getItems().remove(comboZerbitzua.getValue());
+        ZerbitzuKud.getInstance().kenduZerbitzua(comboZerbitzua.getValue().toString());
+    }
+
+    public void setMainApp(Main main) {
+        this.mainApp = main;
+    }
+
+    @FXML
+    public void onClick(ActionEvent actionEvent) {
+
+        comboZerbitzua.getItems().add(comboZerbitzua.getValue());
+        ZerbitzuKud.getInstance().gehitu(comboZerbitzua.getValue().toString());
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        List<String> herrialdeakList = ZerbitzuKud.getInstance().lortuZerbitzuak();
+        ObservableList<String> herrialdeak = FXCollections.observableArrayList(herrialdeakList);
+
+        comboZerbitzua.setItems( herrialdeak );
+        comboZerbitzua.setEditable(true);
+
+    }
+
+}
